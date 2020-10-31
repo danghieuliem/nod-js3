a1719650d9d52ad275d172a5ca3731877754a244
danghieuliem <hieuliem33@gmail.com>
2020-10-31 15:57:09 +0700

    up date weather app :3

--- weatherapp/src/main.js
+++ weatherapp/src/main.js
@@ -13,31 +13,35 @@ app.get('/', (req, res) => {
     const searchkey = req.query.searchkey;
     if (!searchkey) {
         res.render("home", { searchkey });
-        return;
     }
     else {
         weather.searchLocation(searchkey, (error, body) => {
             if (error !== null) {
                 res.render('home', { error, searchkey });
-            } else if (typeof (body[0]) === "object") {
-                var a = 0;
-                body.forEach(element => {
-                    element.min_temp = Math.round(element.min_temp   *100) /100;
-                    element.max_temp = Math.round(element.max_temp   *100) /100;
-                    element.visibility = Math.round(element.visibility *100) /100;
-                });
-                res.render('home', { searchkey, body });
-                console.log("aaaa");
             }
             else {
-                const location = body;
-                console.log("bbbb");
-                res.render('home', { error, searchkey, location });
-
+                res.render('home', { error, searchkey, body });
             }
         });
     }
 });
+app.get('/:woeid', (req, res) =>{
+    const woeid = req.params.woeid;
+    if(!woeid){
+        res.render("home", { woeid, error: "woeid noot found !"});
+    }
+    else {
+        weather.searcheowid(woeid,(error,body)=>{
+            if (error !== null) {
+                res.render('home', { error, woeid });
+            }
+            else {
+                res.render('forecast', { error, woeid, body });
+            }
+        });
+    }
+});
+
 
 app.listen(post, () => {
     console.log(`Runing at : http://localhost:${post}`);
--- weatherapp/src/weather.js
+++ weatherapp/src/weather.js
@@ -9,7 +9,19 @@ const getLocationWeather = (eowid, Callback) => {
             Callback(error = "not found weather !");
         }
         else{
-            body = body.consolidated_weather;
+            body = {
+                title:body.title,
+                weather: body.consolidated_weather.map((item) => {
+                    return {
+                        applicable_date: item.applicable_date,
+                        weather_state_name: item.weather_state_name,
+                        min_temp: Math.round(item.min_temp*100)/100,
+                        max_temp: Math.round(item.max_temp*100)/100,
+                        visibility: Math.round(item.visibility*100)/100,
+                        weather_state_abbr: item.weather_state_abbr
+                    }
+                })
+            }
             Callback(null,body);
         }
     });
@@ -20,17 +32,25 @@ const searchLocation = (key, Callback) => {
         if (error !== null || body.length === 0) {
             Callback(error = "not found location !");
         }
-        else if (body.length > 1) {
-            body = body.map((item) => {return item.title});
+        else if (body.length > 0) {
+            body = body.map((item) => {return { location: item.title, woeid: item.woeid}});
             Callback(null,body);
-        }
-        else{
-            body = body[0];
-            getLocationWeather(body.woeid, Callback);
         }
     });
 }
 
+const searcheowid = (key, Callback) =>{
+    request(`${PATHAPI}location/${key}`, { json: true }, (error, res, body) => {
+        if (error !== null || body.length === 0) {
+            Callback(error = "not found location !");
+        }
+        else{
+            getLocationWeather(body.woeid, Callback);
+        }
+    })
+}
+
 module.exports = {
     searchLocation: searchLocation,
+    searcheowid: searcheowid
 }
